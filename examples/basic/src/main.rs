3a4ef18131a30d982a0580862da975b0011ce992
Daniel Mauderer <maudi@wayblue.fritz.box>
2026-05-03 20:54:56 +0200

    pipeline

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,10 +1,16 @@
+#![no_std]
+#![no_main]
+
 use provisioner::Provision;
 
 #[derive(Provision)]
 struct MyConfig {
-    // ssid: heapless::String<32>,
-    // password: heapless::String<64>,
-    // use_dhcp: bool,
+    ssid: heapless::String<32>,
+    password: heapless::String<64>,
+    use_dhcp: bool,
 }
 
-fn main() {}
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
